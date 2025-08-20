d854031ebdca01bc68ebb12906735a08374f43aa
HectorCacers <hectorcacers@unitec.edu>
2025-08-20 17:01:43 -0600

    Agregados endpoints GET, POST y PUT en app.js

--- heladeria/app.js
+++ heladeria/app.js
@@ -4,12 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index'); // aquí ya se incluyen las demás rutas
 
 var app = express();
 
-// view engine setup
+// view engine setup (si usas vistas Jade/Pug, si no, se puede quitar)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -19,27 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas principales
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// =========================
-// ENDPOINTS DE EJEMPLO "HOLA"
-// =========================
-
-// GET request
-app.get('/hola', (req, res) => {
-  res.send('¡Hola! Esto es un GET');
-});
-
-// POST request
-app.post('/hola', (req, res) => {
-  res.send('¡Hola! Esto es un POST');
-});
-
-// PUT request
-app.put('/hola', (req, res) => {
-  res.send('¡Hola! Esto es un PUT');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
